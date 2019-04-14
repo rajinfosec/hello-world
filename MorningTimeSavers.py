0a621d4ef71e6b2fa3fab7867f51a24a5a8b2e2e
rajinfosec <49372805+rajinfosec@users.noreply.github.com>
2019-04-14 21:26:24 +0530

    Timesavers

---
+++ MorningTimeSavers.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+webbrowser.open('www.google.com')
+webbrowser.open('https://www.codecademy.com/')
+webbrowser.open('https://github.com/')
+
+import os
+os.startfile("outlook")
+os.startfile("Notepad")
